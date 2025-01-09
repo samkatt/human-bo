fe2bd6d0ef50827a2392bde05811fb2baea310d0
Sammie Katt <sammie.katt@aalto.fi>
2025-01-09 15:54:03 +0200

    Make scripts installable

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(
     name="human-in-the-bo",
     packages=["human_bo"],
-    scripts=[],
+    scripts=[
+        "scripts/run_human_ai_experiment.py",
+        "scripts/visualize_human_ai_experiment.py",
+    ],
 )

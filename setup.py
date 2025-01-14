73a7b7ac746cae637b944d64c613e9f84f5ee055
Sammie Katt <sammie.katt@aalto.fi>
2025-01-14 17:01:38 +0200

    Make them proper scripts by removing .py.

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="human-in-the-bo",
     packages=["human_bo"],
     scripts=[
-        "scripts/run_human_ai_experiment.py",
-        "scripts/visualize_human_ai_experiment.py",
+        "scripts/run_human_ai_experiment",
+        "scripts/run_human_then_ai_experiment",
+        "scripts/visualize_human_ai_experiment",
     ],
 )
